93caa55e7632adf08681925595d3c69adbf02f5b
John Park <johnsmp90@gmail.com>
2025-04-12 01:55:17 -0300

    implement activeplayer with usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/Gameboard.jsx';
 
 function App() {
+  const [activePlayer, setActiverPlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActiverPlayer((curActivePlayer) => curActivePlayer === 'X' ? '0' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   )
